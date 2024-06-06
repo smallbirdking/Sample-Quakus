c6b668cb2bb87b19b8fd744855afc292245509d2
Yuqi WANG <Yuqi.WANG@amadeus.com>
2024-06-06 22:55:46 +0800

    ted: suggestions

--- src/test/java/org/acme/getting/started/NativeGreetingResourceIT.java
+++ src/test/java/org/acme/getting/started/NativeGreetingResourceIT.java
@@ -4,6 +4,6 @@ import io.quarkus.test.junit.NativeImageTest;
 
 @NativeImageTest
 public class NativeGreetingResourceIT extends GreetingResourceTest {
-
+    //
     // Execute the same tests but in native mode.
 }
